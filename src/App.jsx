2f1cfae2e156c0105ec30616e0e0ae6b287f821e
dev-fikayomi <fikayopeter@gmail.com>
2024-10-04 23:09:01 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+import reactLogo from '/react.png';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState('');
+  const message = "We are Coming Soon..."; 
+  const typingSpeed = 100; 
+
+ 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (text.length < message.length) {
+        setText((prevText) => prevText + message[text.length]);
+      }
+    }, typingSpeed);
+
+    return () => clearTimeout(timer); 
+  }, [text]);
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+         
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>{text}</h1>
+    </>
+  );
+}
+
+export default App;
